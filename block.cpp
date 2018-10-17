a5e1c15a57dba007c8fde5c83ff5204545d61b9b
jyabad <43794534+jyabad@users.noreply.github.com>
2018-10-16 23:08:46 -0700

    Create block.cpp

---
+++ block.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        cout << "Enter number of rows and columns\n";
+        int rows;
+        int columns;
+        cin >> rows >> columns;
+        int i = 0;
+        while (i<rows)
+        {
+                for (int j=0; j<columns; j++)
+                {
+                        cout << "X.";
+                }
+        cout << "\n";
+        i++;
+        }
+        return 0;
+}
