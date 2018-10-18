c114660d5bdf26b69efe2ef4cf2fa245eccf538d
Justin Dominic Abad <jyabad@csil-01.cs.ucsb.edu>
2018-10-17 22:49:53 -0700

    changes

--- pi.cpp
+++ pi.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 int main()
 {
+	cout.precision(4);
         int parameter;
         double output;
         while (parameter>-1)
         {
                 cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" <<endl ;
                 cin >> parameter;
-                output = 0;
+		output = 0;
                 for (int i=0; i <= parameter; i++)
                 {
-                        if(i % 2 == 0)
+			if(i % 2 == 0)
                         {
                                 output+=1.0/(2.0*i+1.0);
                         }
@@ -23,13 +24,19 @@ int main()
                         }
                 }
                 output*=4.0;
-                if (parameter >=0)
+                if (parameter >=0 && output != 4)
                 {
                         cout << "The approximate value of pi using ";
                         cout << parameter +1;
                         cout << " terms is: ";
-                        cout << output << endl;
+			cout << output << endl;
                 }
+		else if (output = 4)
+		{
+			cout << "The approximate value of pi using ";
+			cout << parameter + 1;
+			cout << " terms is: 4.000" << endl;
+		}
         }
         return 0;
 }
