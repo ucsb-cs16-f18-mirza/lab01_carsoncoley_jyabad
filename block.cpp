273da403e611c58bf78f7380b3eb884f4cf76541
Justin Dominic Abad <jyabad@csil-01.cs.ucsb.edu>
2018-10-17 22:20:18 -0700

    changes

--- block.cpp
+++ block.cpp
@@ -3,19 +3,23 @@ using namespace std;
 
 int main()
 {
-        cout << "Enter number of rows and columns:\n";
-        int rows;
-        int columns;
-        cin >> rows >> columns;
-        int i = 0;
-        while (i<rows)
-        {
-                for (int j=0; j<columns; j++)
-                {
-                        cout << "X.";
-                }
-        cout << "\n";
-        i++;
-        }
+        int rows = 1;
+        int columns = 1;
+	while (rows > 0 && columns > 0)
+	{	
+		cout << "Enter number of rows and columns:\n";
+		cin >> rows >> columns;
+		int i = 0;
+		int j = 0;
+		while (i<rows)
+        	{
+                	for (int j=0; j<columns; j++)
+                	{
+                        	cout << "X.";
+                	}
+        	cout << "\n";
+        	i++;
+        	}
+	}
         return 0;
 }
